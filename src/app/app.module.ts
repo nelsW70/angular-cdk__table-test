f47c7cd17466306a74f49de7cc2bbdc44eb25eea
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-09-05 17:01:52 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+// Material Modules
+import { MatToolbarModule, MatTableModule } from '@angular/material';
+import { TableComponent } from './table/table.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatTableModule
   ],
-  providers: [],
+  declarations: [AppComponent, TableComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
